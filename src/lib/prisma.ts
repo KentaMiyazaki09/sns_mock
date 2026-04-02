0dad96ff0630a260fc059a78ec77fa0d0b43c1fb
KentaMiyazaki09 <kmiyazaki0924@gmail.com>
2026-04-02 19:05:17 +0900

    postgrace用に書き換え

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,16 +3,14 @@
  * このファイルでは、Prismaクライアントをシングルトンとして定義し、アプリケーション全体で共有できるようにしています。
  */
 
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as {
   prisma?: PrismaClient;
 };
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL!
-})
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({adapter});
 
