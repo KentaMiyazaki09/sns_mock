afc6e429063791d57f58b0c94fc7fe4998271c76
KentaMiyazaki09 <kmiyazaki0924@gmail.com>
2026-05-17 16:16:44 +0900

    コメント追加

--- auth.ts
+++ auth.ts
@@ -1,3 +1,11 @@
+// ログインからそれぞれのAPI実行までの流れ
+// 1. GitHubログイン成功
+// 2. jwt callback で token.id を保存
+// 3. session callback で session.user.id を作る
+// 4. page.tsx や API で auth() を呼ぶ
+// 5. session.user.id を currentUser.id として使う
+// 6. 投稿作成・削除の認可に使う
+
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { DefaultSession } from "next-auth";
@@ -37,6 +45,9 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     signIn: "/login",
   },
   callbacks: {
+    // tokenにGithub OAuthから取得したユーザー識別情報を保存
+    // tokenは「Auth.js が内部で持っているログイン状態のデータ箱」
+    // idをsessionを作るときに再利用が可能
     jwt({ token, account }) {
       if (account?.provider === "github" && account.providerAccountId) {
         token.id = account.providerAccountId;
@@ -48,6 +59,8 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
 
       return token;
     },
+
+    // tokenをclient/serverから使いやすい形に変形
     session({ session, token }) {
       if (session.user && token.id) {
         session.user.id = String(token.id);
